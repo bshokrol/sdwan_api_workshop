e91bb2c4150d2cd896b3d216b8e2419584b6b4aa
Bijan Shokrollahi (bshokrol) <bshokrol@cisco.com>
2021-04-24 14:47:25 -0700

    fixed df display

--- sdwan_bootcamp_lab.py
+++ sdwan_bootcamp_lab.py
@@ -4,18 +4,19 @@ created by bshokrol
 04/24/2021
 
 """
-#ignore insecure requests warning
+
+# pip install pandas
 import pandas as pd
 import sys
 import getpass
+# pip install requests
 import requests
+# ignore insecure requests warning
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-#pip install requests
-#pip install pandas
 pd.set_option('display.max_rows', 50)
-pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_columns', 10)
 pd.set_option('display.width', 2000)
 
 GET_ALL_DEVICES = "dataservice/device"
@@ -59,8 +60,8 @@ class SdwanBootcamp:
 
 
 def main() -> None:
-    #instructions fill in the missing code wherever you see a _____
-    #1 initialize class with credentials + authenticate
+    # instructions fill in the missing code wherever you see a _____
+    # 1 initialize class with credentials + authenticate
     vmanage_ip = ""
     vmanage_port = ""
     vmanage_username = ""
@@ -68,19 +69,19 @@ def main() -> None:
     vmanage_address = f"https://{vmanage_ip}:{vmanage_port}/"
     sdwan_bootcamp = SdwanBootcamp(vmanage_address=vmanage_address,
                                    vmanage_username=vmanage_username, vmanage_password=vmanage_password)
-    #2 get all devices in overlay using get_request
+    # 2 get all devices in overlay using get_request
     _____ = sdwan_bootcamp.get_request(endpoint=_____)
     print(_____.status_code)
     all_devices_df = pd.DataFrame(_____.json().get("data"))
     print("All devices dataframe")
     print(all_devices_df)
-    #3 get all device templates in vmange using get_request
+    # 3 get all device templates in vmange using get_request
     response = sdwan_bootcamp._____(endpoint=GET_ALL_DEVICE_TEMPLATES)
     print(response.status_code)
     all_device_templates_df = pd.DataFrame(response._____._____("data"))
     print("All device templates dataframe")
     print(all_device_templates_df)
-    #4 get current bfd session for a specific device using get_request
+    # 4 get current bfd session for a specific device using get_request
 
 
 if __name__ == "__main__":
